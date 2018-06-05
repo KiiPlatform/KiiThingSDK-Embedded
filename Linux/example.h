1d8c9fd45ae9fb466103568857debc71ad61daa2
yongpingchen <yongping.chen@kii.com>
2018-06-05 12:53:31 +0900

    test with linux sample

--- Linux/example.h
+++ Linux/example.h
@@ -9,8 +9,8 @@ const char EX_AUTH_VENDOR_PASS[] = "1234";
 const char EX_AUTH_VENDOR_TYPE[] = "my_type";
 
 const char EX_APP_SITE[] = "JP";
-const char EX_APP_ID[] = "9ab34d8b";
-const char EX_APP_KEY[] = "7a950d78956ed39f3b0815f0f001b43b";
+const char EX_APP_ID[] = "krwxhgkauf70";
+const char EX_APP_KEY[] = "f7e5af0a5d7f4cd88682b94462264ac5";
 
 #define EX_BUFFER_SIZE 4096
 #define EX_MQTT_BUFFER_SIZE 2048
@@ -21,9 +21,9 @@ const char EX_OBJECT_ID[] = "my_object";
 const char EX_BUCKET_NAME[] = "my_bucket";
 const char EX_TOPIC_NAME[] = "my_topic";
 const char EX_OBJECT_DATA[] = "{}";
-const char EX_BODY_DATA[] = "Hello world !\n";
+const char EX_BODY_DATA[] = "Hello world !sfsdfdfdsfsdfsdfsdfdsfdsfsdffsdfd\n";
 const char EX_ENDPOINT_NAME[] = "test_topic";
-
+const char EX_FILE_TO_SAVE[] = "object-body";
 #ifdef __cplusplus
 }
 #endif
