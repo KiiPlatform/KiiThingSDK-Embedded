f7c01f6b86a026523ebcadf2108188b25b6fe1d7
yongpingchen <yongping.chen@kii.com>
2018-07-23 16:47:58 +0900

    improve open file callback: - if file with same name exsiting, delete first - open file with append mode

--- Linux/kii_file_impl.c
+++ Linux/kii_file_impl.c
@@ -8,7 +8,13 @@
 int fd = -1;
 
 kii_file_code_t kii_file_open() {
-    fd = open(EX_OBJECT_BODY_FILE_NAME, O_WRONLY | O_CREAT);
+    if ( access(EX_OBJECT_BODY_FILE_NAME, F_OK) != -1) {
+        if (unlink(EX_OBJECT_BODY_FILE_NAME) != 0) {
+            printf("faild to remove existing body file\n");
+            return KII_FILE_FAIL;
+        }
+    }
+    fd = open(EX_OBJECT_BODY_FILE_NAME, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (fd == -1) {
         return KII_FILE_FAIL;
     }
